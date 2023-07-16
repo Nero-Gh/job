af09af9aca8a5fd264124f0745903429b3d0b28d
nero-gh <neroaboagye@gmail.com>
2023-07-16 16:59:28 +0000

    redux added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
+import { Box } from "@mui/material";
 import Header from "../components/Header";
 import NavBar from "../components/Navbar";
 
 const Home = () => {
   return (
     <>
-      <NavBar />
-      <Header />
+      <Box sx={{ bgColor: "#fafafa", minHeight: "100vh " }}>
+        <NavBar />
+        <Header />
+      </Box>
       <h1>Home</h1>
     </>
   );
